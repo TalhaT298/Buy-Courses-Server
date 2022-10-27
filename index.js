8064494397f93bed8db82e40dc0454a910c6bef3
Talha <mdtalha2008@gmail.com>
2022-10-27 22:43:11 +0600

    start

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const port = process.env.PORT ||5000;
+const port = 5000;
+const cors = require("cors");
+const courses = require("./data/courses.json");
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.get("/courses", (req, res) => {
+    res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(courses.id);
+    const selectedCourse = courses.find((course) => course.id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port `,port);
-})
+    console.log(`Port is running on ${port}`);
+});
+
